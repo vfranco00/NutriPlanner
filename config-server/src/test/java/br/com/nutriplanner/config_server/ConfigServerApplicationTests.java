3255196d1820f592c13775f368913db31ddc91bf
Franco <victorfranco02@outlook.com>
2025-07-10 18:21:24 -0300

    feat: config_server implementatio

---
+++ config-server/src/test/java/br/com/nutriplanner/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nutriplanner.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
