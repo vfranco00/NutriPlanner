edbb67b47134f158070082fdb1f225ec9efe46b0
Franco <victorfranco02@outlook.com>
2025-07-14 20:14:41 -0300

    fix: dockerfile change implementation

--- discovery-server/src/main/java/br/com/nutriplanner/discovery_server/DiscoveryServerApplication.java
+++ discovery-server/src/main/java/br/com/nutriplanner/discovery_server/DiscoveryServerApplication.java
@@ -2,10 +2,8 @@ package br.com.nutriplanner.discovery_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableDiscoveryClient
 @EnableEurekaServer
 @SpringBootApplication
 public class DiscoveryServerApplication {
